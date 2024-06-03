640676c105bd2891e26ef4b60c9ed78ecbdd62c5
MaxCosmos <renyh2001@outlook.com>
2024-06-03 22:01:39 +0800

    【Enemy】完成敌人飞机动态添加到主场景中

--- scripts/main/Main.gd
+++ scripts/main/Main.gd
@@ -1,13 +1,19 @@
 extends Node
 
-
+@export var enemy_scene: PackedScene
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$BackgroundImg.start()
 	$Player.start($StartPosition.position)
-	pass # Replace with function body.
+	$EnemyTimer.start()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_enemy_timer_timeout():
+	var enemy = enemy_scene.instantiate()
+	$Enemys.add_child(enemy)
