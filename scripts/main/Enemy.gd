f0647b10a7d7fdc76955e183f2fa4311f6349f2e
MaxCosmos <renyh2001@outlook.com>
2024-06-03 21:52:46 +0800

    【Enemy】完成敌人飞机的生成和销毁

---
+++ scripts/main/Enemy.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+@export var speed = 400
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite2D.play("fly")
+	# 随机选择一个位置
+	position.y = 0
+	position.x = randi() % 480
+	# 随机选择速度
+	speed = randi() % 200 + 100
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	velocity.y = 1
+	velocity = velocity * speed
+	position = position + velocity * delta
+	
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
