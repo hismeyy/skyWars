bbf1fdf3b7b9e2520c3e4d3629f20c0b61d228b7
MaxCosmos <renyh2001@outlook.com>
2024-06-03 21:04:54 +0800

    【main】测试主场景，测试player移动，修复bug，优化代码

--- scripts/entities/Player.gd
+++ scripts/entities/Player.gd
@@ -4,7 +4,7 @@ extends Area2D
 var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimatedSprite2D.play("fly")
+	hide()
 	screen_size = get_viewport_rect().size
 
 
@@ -12,16 +12,22 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("left_move"):
-		velocity -= 1
+		velocity.x -= 1
 	if Input.is_action_pressed("right_move"):
-		velocity += 1
+		velocity.x += 1
 	if Input.is_action_pressed("up_move"):
-		velocity -= 1
+		velocity.y -= 1
 	if Input.is_action_pressed("down_move"):
-		velocity += 1
+		velocity.y += 1
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 	
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionPolygon2D.disabled = false
+	$AnimatedSprite2D.play("fly")
--- scripts/main/Main.gd
+++ scripts/main/Main.gd
@@ -1,14 +1,13 @@
-extends AnimationPlayer
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	$BackgroundImg.start()
+	$Player.start($StartPosition.position)
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func start():
-	play("backgroundPlay")
